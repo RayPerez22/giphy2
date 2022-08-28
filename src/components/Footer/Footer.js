621df2692c67b26faecf8370df15a6dd9b1414db
Iskandar <30454668+Iskandar-S@users.noreply.github.com>
2022-08-28 14:14:45 -0400

    added footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+function Footer() {
+	return (
+		<div>
+			<h3>
+				<i className="twa twa-copyright"></i> 2022
+			</h3>
+			<h3>
+				<i className="twa twa-high-voltage"></i> by Passion
+			</h3>
+			<h3>
+				Coded by{' '}
+				<a
+					href="https://github.com/suzannemozes"
+					target="_blank"
+					rel="noreferrer">
+					Suzanne
+				</a>
+				,{' '}
+				<a
+					href="https://github.com/cyanghan007"
+					target="_blank"
+					rel="noreferrer">
+					Chan
+				</a>
+				,{' '}
+				<a
+					href="https://github.com/RayPerez22"
+					target="_blank"
+					rel="noreferrer">
+					Raymond
+				</a>{' '}
+				and{' '}
+				<a
+					href="https://github.com/Iskandar-S"
+					target="_blank"
+					rel="noreferrer">
+					Iskandar
+				</a>
+			</h3>
+			<h3>
+				Developers from NYC{' '}
+				<i className="twa twa-lg twa-red-apple"></i>
+			</h3>
+		</div>
+	);
+}
+
+export default Footer;
