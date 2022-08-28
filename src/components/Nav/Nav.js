c3f93eebe0ff98209db0d4587e4ec3c3676f13cb
Iskandar <30454668+Iskandar-S@users.noreply.github.com>
2022-08-28 14:15:09 -0400

    added Nav

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Nav() {
+	return (
+		<div className="Nav">
+			<a href="/">Home</a>
+			<a href="/about">About</a>
+			<a href="/lab">Lab</a>
+		</div>
+	);
+}
+
+export default Nav;
