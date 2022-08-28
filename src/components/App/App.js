e8bab5372d5000f147ce304eca8ad7b6a45f124b
Iskandar <30454668+Iskandar-S@users.noreply.github.com>
2022-08-28 14:16:23 -0400

    linked Nav and Footer components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,30 +1,29 @@
-import { useState } from 'react';
-import axios from 'axios'
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+} from 'react-router-dom';
+import About from '../../pages/About/About';
 import Home from '../../pages/Home/Home';
 import Lab from '../../pages/Lab/Lab';
-import About from '../../pages/About/About';
-
+import Footer from '../Footer/Footer';
+import Nav from '../Nav/Nav';
+import './App.css';
 
 function App() {
-  
-
-  return (
-    <div className="App">
-      <h1>Giphy!</h1>
-      
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/about' element={<About />}/>
-          <Route path='/lab' element={<Lab />}/>
-        </Routes>
-      </Router>
-      
-
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Nav />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/lab" element={<Lab />} />
+				</Routes>
+				<Footer />
+			</Router>
+		</div>
+	);
 }
 
 export default App;
