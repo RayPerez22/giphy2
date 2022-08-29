2bb28057aa306c852b13feebdb44ccfc51e96b33
Iskandar <30454668+Iskandar-S@users.noreply.github.com>
2022-08-28 23:33:00 -0400

    added content, linked css

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './Home.css';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+	return (
+		<div className="home container-fluid text-bg-dark">
+			<div className="fullscreen-bg"></div>
+			<div className="home-text position-absolute bottom-50 start-50 translate-middle-x">
+				<h1 className="display-1 fw-normal">
+					Welcome to our React Router Giphy Lab!
+				</h1>
+			</div>
+		</div>
+	);
+};
 
-export default Home
+export default Home;
