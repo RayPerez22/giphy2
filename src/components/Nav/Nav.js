f02ac4393a18b24a4d9a4650c4e9515483b06a08
Iskandar <30454668+Iskandar-S@users.noreply.github.com>
2022-08-28 23:28:36 -0400

    finished navbar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,11 +2,44 @@ import React from 'react';
 
 function Nav() {
 	return (
-		<div className="Nav">
-			<a href="/">Home</a>
-			<a href="/about">About</a>
-			<a href="/lab">Lab</a>
-		</div>
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+			<div className="container-fluid">
+				<a className="navbar-brand" href="/">
+					<img src="/logo.gif" alt="" width="40" height="34" />
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div
+					className="collapse navbar-collapse"
+					id="navbarSupportedContent">
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+						<li className="nav-item">
+							<a className="nav-link" href="/">
+								Home
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="/about">
+								About
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="/lab">
+								Giphy Lab
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
 	);
 }
 
